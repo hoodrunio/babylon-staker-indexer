74f1aa50e8daa6a92d5b8858df3f78cb89c4e03e
Errorist <71728280+Errorist79@users.noreply.github.com>
2024-12-22 05:44:36 +0300

    Enable proxy trust

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import { BabylonIndexer } from './services/BabylonIndexer';
 dotenv.config();
 
 const app = express();
+app.set('trust proxy', true);
 const port = process.env.PORT || 3000;
 
 // Middleware
