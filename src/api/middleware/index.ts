4e68515f95fa0d69fb9d6320d9809013f39d3f63
Errorist <71728280+Errorist79@users.noreply.github.com>
2025-01-16 20:38:30 +0300

    Updated rateLimiter config to 5 mins and 200 reqs per IP

--- src/api/middleware/index.ts
+++ src/api/middleware/index.ts
@@ -34,8 +34,8 @@ export const compressionMiddleware = compression();
 
 // Rate limiting middleware
 export const rateLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100 // limit each IP to 100 requests per windowMs
+  windowMs: 5 * 60 * 1000, // 5 minutes
+  max: 200 // limit each IP to 100 requests per windowMs
 });
 
 // Pagination middleware
