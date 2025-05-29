0e3cfb6ca0aa289fc7b899a72ca57c574b77f27e
Errorist <71728280+Errorist79@users.noreply.github.com>
2025-05-29 12:31:16 +0300

    Remove IBC relayer routes and controller

--- src/api/routes/v1/ibc/index.ts
+++ src/api/routes/v1/ibc/index.ts
@@ -4,7 +4,6 @@ import channelsRouter from './channels';
 import packetsRouter from './packets';
 import connectionsRouter from './connections';
 import clientsRouter from './clients';
-import relayersRouter from './relayers';
 import analyticsRouter from './analytics.routes';
 
 const router = Router();
@@ -15,7 +14,6 @@ router.use('/channels', channelsRouter);
 router.use('/packets', packetsRouter);
 router.use('/connections', connectionsRouter);
 router.use('/clients', clientsRouter);
-router.use('/relayers', relayersRouter);
 router.use('/analytics', analyticsRouter);
 
 export default router; 
